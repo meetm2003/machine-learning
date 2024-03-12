eded0edc437c873517d7897c64d8e27b033f8684
meetm2003 <meetm2003@gmail.com>
2024-03-11 23:51:57 -0700

    your commit message

---
+++ lab 5/bagged_decision_tree.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 23:26:10 2024
+
+@author: student
+"""
+
+from pandas import read_csv
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+filename = 'pima-indians-diabetes.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = read_csv(filename, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+seed = 7
+kfold = KFold(n_splits=10, random_state=seed, shuffle=True)
+cart = DecisionTreeClassifier()
+num_trees = 100
+model = BaggingClassifier(estimator=cart, n_estimators=num_trees, random_state=seed)
+results = cross_val_score(model, X, Y, cv=kfold)
+print(results.mean())
